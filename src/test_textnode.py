3fa75386b2cf395db66e2044f05653ccca62ddf6
araj-abood <alaraj.abdullah02@gmail.com>
2025-11-29 12:11:03 +0300

    add another test case

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -14,6 +14,12 @@ class TestTextNode(unittest.TestCase):
         node1 = TextNode("This is a text node", TextNode.TextType.BOLD, "I dont have a url")
         
         self.assertNotEqual(node, node1)
+        
+    def test_not_equal_when_node_1_not_text_node(self):
+        node = TextNode("This is a text node", TextNode.TextType.BOLD, "i have a url")
+        node1 = "I  am not a text node"
+        
+        self.assertNotEqual(node, node1)
 
 if __name__ == "__main__":
     unittest.main()
